ba5ce2a360aee2bb3e9fb7085a2f2c4535870ca5
daniel <menacanaviridaniel@gmail.com>
2019-02-02 01:45:16 -0400

    Septima sesión parte 1

---
+++ 7. Dibujos html/canvas.js
@@ -0,0 +1,25 @@
+/*
+    Introducción a JavaScript
+
+    JS es un lenguaje de programación del lado del cliente (significa que el navegador lo interpreta).
+
+    VARIABLES
+
+    Son contenedores para valores.
+
+        var profesor = "Juan Perez";
+
+    OBJETOS
+
+    Casi todo en JS es un objeto. Los objetos tienen propiedades y métodos. Las propiedades son caracteristicas mientras que los métodos son funciones (hacen algo).
+    Ejemplo:
+
+        profesor.edad    (propiedad)
+        profesor.caminar (método)
+
+*/
+
+var profesor = "Juan Perez";
+
+alert (profesor);
+console.log("Mi nombre es "+profesor);
